16aebbb7d13586bd766c5682d188a55f261182cf
Chafik <elghandour.chafik@gmail.com>
2015-11-21 15:04:49 +0100

    REQ_001

---
+++ CarReqSteps.java
@@ -0,0 +1,18 @@
+import org.jbehave.core.annotations.*;
+public class CarReqSteps{
+	@Then("the light should be ON")
+	@Pending
+	public void thenTheLightShouldBeON(){
+		 //TODO 
+	}
+	@When("when the switch mode is ON")
+	@Pending
+	public void whenWhenTheSwitchModeIsON(){
+		 //TODO 
+	}
+	@Given("a car light system")
+	@Pending
+	public void givenACarLightSystem(){
+		 //TODO 
+	}
+}
